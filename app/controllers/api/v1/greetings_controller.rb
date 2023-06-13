126b9e8ffd52922d743150eb5881c8efd2fb9022
otmanTR <r.brk.otman@gmail.com>
2023-06-13 11:06:05 +0300

    Fix bugs

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     random_greeting = Greeting.order('RANDOM()').first
-    render json: { message: random_greeting.text }
+    render json: { message: random_greeting.message }
   end
 end
